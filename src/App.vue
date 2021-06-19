6994e90ebc4d90110fc15d2764e7817015ce57d0
ayoub3bidi <mrayoubabidi@gmail.com>
2021-06-19 15:06:15 +0100

    Firebase setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="row">
-        <router-view/>
+      <router-view />
     </div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <p>Home page</p>
   </div>
 </template>
 

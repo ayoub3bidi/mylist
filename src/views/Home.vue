47737a93a5727521635760310039b63239ba2a7d
ayoub3bidi <mrayoubabidi@gmail.com>
2021-06-27 14:48:47 +0100

    Getting real-time lists data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="home">
+  <div>
+    <div v-if="error" class="text-danger">Could not fetch the data</div>
+    <div v-if="documents">
+      <div v-for="doc in documents" :key="doc.id">{{ doc.title }}</div>
+    </div>
   </div>
 </template>
 
 <script>
+import getCollection from '../composables/getCollection'
 
 export default {
   name: 'Home',
-  components: {}
+  setup() {
+    const { error, documents } = getCollection('Lists')
+
+    return { error, documents }
+  }
 }
 </script>

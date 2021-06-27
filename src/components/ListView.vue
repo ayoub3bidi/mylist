58e8a446a9908734c4250e8c6483e437f0cc2d07
ayoub3bidi <mrayoubabidi@gmail.com>
2021-06-27 15:51:23 +0100

    Listing lists

---
+++ src/components/ListView.vue
@@ -0,0 +1,59 @@
+<template>
+    <div v-for="List in Lists" :key="List.id">
+        <br>
+        <div class="card">
+            <div class="card-body">
+                <div class="single">
+                    <div class="thumbnail">
+                        <img :src="List.coverUrl">
+                    </div>
+                    <div class="info">
+                        <h3>{{List.title}}</h3>
+                        <p>{{List.description}}</p>
+                        <p>Created by {{List.userName}}</p>
+                    </div>
+                    <div class="item-number">
+                        <p>{{List.items.length}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['Lists']
+
+    }
+</script>
+
+<style scoped>
+.single {
+    display: flex;
+    align-items: center;
+    transition: all ease 0.2s;
+  }
+  .single:hover {
+    box-shadow: 1px 2px 3px rgba(50,50,50,0.05);
+    transform: scale(1.02);
+    transition: all ease 0.2s;
+  }
+  .thumbnail {
+    max-width: 200px;
+    max-height: 200px;
+    overflow: hidden;
+    border-radius: 5px;
+  }
+  img {
+    max-width: 100%;
+    max-height: 100%;
+    display: block;
+  }
+  .info {
+    margin: 0 30px;
+  }
+  .item-number {
+    margin-left: auto;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,18 @@
   <div>
     <div v-if="error" class="text-danger">Could not fetch the data</div>
     <div v-if="documents">
-      <div v-for="doc in documents" :key="doc.id">{{ doc.title }}</div>
+      <ListView :Lists="documents" />
     </div>
   </div>
 </template>
 
 <script>
+import ListView from '../components/ListView.vue'
 import getCollection from '../composables/getCollection'
 
 export default {
   name: 'Home',
+  components: { ListView },
   setup() {
     const { error, documents } = getCollection('Lists')
 

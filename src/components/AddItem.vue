61b7a5de807ac760b0304407b645547438cba6a9
ayoub3bidi <mrayoubabidi@gmail.com>
2021-07-03 13:45:44 +0100

    Creating AddItem component

---
+++ src/components/AddItem.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <button v-if="!showForm" class="btn btn-outline-dark" @click="showForm = true" >Add a new item</button>
+        <div v-if="showForm"  class="card">
+            <div class="card-header">
+                <h3 class="display-4">Add a new item</h3>
+            </div>
+            <div class="card-body">
+                <form @submit.prevent="handleSubmit">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Item name" required v-model="itemName">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Author, Artist, Director..." required v-model="author">
+                    </div>
+                    <button class="btn btn-outline-dark">Add</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+    export default {
+        setup() {
+            const itemName = ref('')
+            const author = ref('')
+            const showForm = ref(false)
+
+            const handleSubmit = async () => {
+                const newItem = {
+                    itemName: itemName.value,
+                    author: author.value,
+                    id: Math.floor(Math.random() * 1000000)
+                }
+                console.log(newItem)
+            }
+
+            return { itemName, author, showForm, handleSubmit }
+        }
+
+    }
+</script>
+
+<style>
+
+</style>
--- src/views/lists/ListDetails.vue
+++ src/views/lists/ListDetails.vue
@@ -6,9 +6,9 @@
       <div class="card-header">
         <h4 class="text-center">{{ List.title }}</h4>
         <img class="img-fluid rounded" :src="List.coverUrl">
+        <p style="margin: 10px" class="text-center">Created by {{ List.userName }}</p>
       </div>
-      <p class="text-center">Created by {{ List.userName }}</p>
-      <p class="text-center">{{ List.description }}</p>
+      <p style="margin: 10px" class="text-center">{{ List.description }}</p>
       <div class="card-footer text-center">
         <button v-if="ownership" @click="handleDelete" class="btn btn-outline-dark">Delete List</button>
       </div>
@@ -17,14 +17,13 @@
   <div v-if="List" class="col-lg-9">
     <br>
     <!-- List items -->
-    <div class="card">
-      <p>qdsfqsdf</p>
-    </div>
+    <AddItem v-if="ownership" :List="List" />
     <div v-if="error" class="text-danger">{{ error }}</div>
   </div>
 </template>
 
 <script>
+import AddItem from '@/components/AddItem.vue'
 import useStorage from '@/composables/useStorage'
 import useDocument from '@/composables/useDocument'
 import getDocument from '@/composables/getDocument'
@@ -34,6 +33,7 @@ import { useRouter } from 'vue-router'
 
 export default {
     props: ['id'],
+    components: { AddItem },
     setup(props) {
       const { error, document: List } = getDocument('Lists', props.id)
       const { user } = getUser()
